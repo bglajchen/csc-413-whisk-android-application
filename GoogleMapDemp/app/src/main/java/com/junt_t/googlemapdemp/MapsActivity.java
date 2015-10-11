0e53e84cab90fbcecbca8b323a7c34b51f81b35b
Junten <oranget361@gmail.com>
2015-10-11 05:03:42 -0700

    add search and mylocation

--- GoogleMapDemp/app/src/main/java/com/junt_t/googlemapdemp/MapsActivity.java
+++ GoogleMapDemp/app/src/main/java/com/junt_t/googlemapdemp/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.junt_t.googlemapdemp;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,6 +13,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.junt_t.googlemapdemp.R.id.TFaddress;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,15 +33,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    public void onSearch(View view) {
+        EditText location_tf = ( EditText)findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        List<Address> addressList = null;
+
+        if(location != null || !location.equals("")) {
+            Geocoder geocode = new Geocoder(this);
+            try {
+               addressList = geocode.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
