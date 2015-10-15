bd133cf3fb407edc893d75059bd69fa2ea325a34
Brandon Glajchen <brandonglajchen@gmail.com>
2015-10-15 10:37:27 -0700

    First working version of api_recipepuppy. Prints a basic JSON object from a hard-coded URL string.

--- api_recipepuppy/api_recipepuppy/app/src/main/java/com/example/brandonglajchen/api_recipepuppy/MainActivity.java
+++ api_recipepuppy/api_recipepuppy/app/src/main/java/com/example/brandonglajchen/api_recipepuppy/MainActivity.java
@@ -1,20 +1,32 @@
 package com.example.brandonglajchen.api_recipepuppy;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    TextView api_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        api_result = (TextView) findViewById(R.id.api_result);
+
+        apiTask task = new apiTask();
+        task.execute("http://www.recipepuppy.com/api/?i=chocolate");
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -34,5 +46,43 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }*/
+
+    public class apiTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection conn = null;
+
+            try {
+                url = new URL(urls[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStream iStream = conn.getInputStream();
+                InputStreamReader isReader = new InputStreamReader(iStream);
+                int data = isReader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = isReader.read();
+                }
+
+                return result;
+
+            } catch (Exception e) {
+                e.getMessage();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            api_result.setText(result);
+        }
     }
 }
